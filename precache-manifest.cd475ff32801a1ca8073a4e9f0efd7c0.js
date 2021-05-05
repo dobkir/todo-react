c4f0d40da70caccca4d46a074ab0dfe145b10e4f
dobkir <dobkir@gmail.com>
2021-05-05 15:37:21 +0400

    Updates

---
+++ precache-manifest.cd475ff32801a1ca8073a4e9f0efd7c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb4aefac60263e614f12dd35911576c",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "a02a7b23f3512e0051bf",
+    "url": "/todo-react/static/css/main.2b80aed3.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "/todo-react/static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-react/static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a02a7b23f3512e0051bf",
+    "url": "/todo-react/static/js/main.3d7d3d7e.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
